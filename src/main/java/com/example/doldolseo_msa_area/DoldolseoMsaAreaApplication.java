e518adf2aa4bb223bb2d03abf07397d05b868fd8
kki7823 <kjd55399997@gmail.com>
2021-11-20 15:43:03 +0900

    Initial commit

---
+++ src/main/java/com/example/doldolseo_msa_area/DoldolseoMsaAreaApplication.java
@@ -0,0 +1,13 @@
+package com.example.doldolseo_msa_area;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoldolseoMsaAreaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoldolseoMsaAreaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/doldolseo_msa_area/DoldolseoMsaAreaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.doldolseo_msa_area;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoldolseoMsaAreaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
