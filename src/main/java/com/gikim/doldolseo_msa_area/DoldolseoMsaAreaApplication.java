fa735c6bae62e58a59d8990e0f4fb82ac716177d
kki7823 <kjd55399997@gmail.com>
2021-11-25 18:16:19 +0900

    211125-지역게시판 업데이트 - war 빌드 관련 설정 추가4

--- src/main/java/com/gikim/doldolseo_msa_area/DoldolseoMsaAreaApplication.java
+++ src/main/java/com/gikim/doldolseo_msa_area/DoldolseoMsaAreaApplication.java
@@ -3,9 +3,9 @@ package com.gikim.doldolseo_msa_area;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@SpringBootApplication
-//public class DoldolseoMsaAreaApplication {
-//    public static void main(String[] args) {
-//        SpringApplication.run(DoldolseoMsaAreaApplication.class, args);
-//    }
-//}
+@SpringBootApplication
+public class DoldolseoMsaAreaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DoldolseoMsaAreaApplication.class, args);
+    }
+}
--- src/main/java/com/gikim/doldolseo_msa_area/ServletInitializer.java
+++ src/main/java/com/gikim/doldolseo_msa_area/ServletInitializer.java
@@ -5,15 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
 public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(ServletInitializer.class);
-    }
-
-    public static void main(String[] args) {
-        SpringApplication.run(ServletInitializer.class, args);
     }
 }
